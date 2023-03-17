bf9da0e8117cc2cf537848789dae72fa0e244ad6
st4pzz <ramellajr@gmail.com>
2023-03-16 23:40:46 -0300

    girando

--- demo.py
+++ demo.py
@@ -1,17 +1,16 @@
 import numpy as np
 import cv2 as cv
 
-#função para rotacionar a imagem para a esquerda ou direita
-def rotate_image(matriz_imagem):
-
-    T = np.array([[1, 0, -150], [0, 1, -150], [0, 0,1]])
-    R = np.array([[0.7, -0.7, 0], [0.7, 0.7, 0], [0, 0,1]])
-    matriz_imagem = np.linalg.inv(T)@R@T@matriz_imagem
-    matriz_imagem = matriz_imagem.astype(int)
-    return matriz_imagem
-   
-
 # Instalar a biblioteca cv2 pode ser um pouco demorado. Não deixe para ultima hora!
+
+#função importada do notebook da aula da matéria que cria os índices para a matriz do frame da imagem
+def criar_indices(min_i, max_i, min_j, max_j):
+    import itertools
+    L = list(itertools.product(range(min_i, max_i), range(min_j, max_j)))
+    idx_i = np.array([e[0] for e in L])
+    idx_j = np.array([e[1] for e in L])
+    idx = np.vstack( (idx_i, idx_j) )
+    return idx
 
 def run():
     # Essa função abre a câmera. Depois desta linha, a luz de câmera (se seu computador tiver) deve ligar.
@@ -27,6 +26,8 @@ def run():
         print("Não consegui abrir a câmera!")
         exit()
 
+    matriz_rotação_incrementa = np.eye(3)
+    matriz_rotação = np.array([[np.cos(np.radians(10)), -np.sin(np.radians(10)), 0], [np.sin(np.radians(10)), np.cos(np.radians(10)), 0],  [0, 0,1]])
     # Esse loop é igual a um loop de jogo: ele encerra quando apertamos 'q' no teclado.
     while True:
         # Captura um frame da câmera
@@ -41,31 +42,40 @@ def run():
         # nas próximas etapas
         frame = cv.resize(frame, (width,height), interpolation =cv.INTER_AREA)
 
-
         # A variável image é um np.array com shape=(width, height, colors)
         image = np.array(frame).astype(float)/255
+        image_ = np.zeros_like(image)
+        Xd= criar_indices(0, image.shape[0], 0, image.shape[1])
+        Xd = np.vstack((Xd,np.ones((1,Xd.shape[1]))))
+        matriz_translação = np.array([[1, 0, -(image.shape[0]/2)], [0, 1, -(image.shape[1]/2)], [0, 0,1]])        
+        matriz_transformação = np.linalg.inv(matriz_translação) @ matriz_rotação_incrementa @ matriz_translação
+        X = np.linalg.inv(matriz_transformação) @ Xd
+        X = X.astype(int)
+        Xd = Xd.astype(int)
+        # Criar um filtro para remover os pontos que não estão na imagem
+        filtro = (X[0,:]>=0)&(X[0,:]<image_.shape[0])&(X[1,:]>=0)&(X[1,:]<image_.shape[1])
+        Xd = Xd[:,filtro]
+        X = X[:,filtro]
+        # Atribuir os valores da imagem original para a imagem transformada
+        image_[Xd[0,:], Xd[1,:], :] = image[X[0,:], X[1,:], :]
+        
 
-        # Aqui, você pode fazer qualquer coisa com a imagem!
-        # Por exemplo, podemos inverter as cores:
-        
-        
-        
-            
-        
-        image = rotate_image(image)
 
-
-
-        # Agora, mostrar a imagem na tela!
-        cv.imshow('Minha Imagem!', image)
-        
         # Se aperto 'q', encerro o loop
         if cv.waitKey(1) == ord('q'):
             break
+        elif cv.waitKey(1) == ord('a'):
+            matriz_rotação_incrementa = matriz_rotação @ matriz_rotação_incrementa  
+        elif cv.waitKey(1) == ord('d'):
+            matriz_rotação_incrementa = np.linalg.inv(matriz_rotação) @ matriz_rotação_incrementa
+        # Agora, mostrar a imagem na tela!
+        cv.imshow('Minha Imagem!', image_)
+        
+        
 
     # Ao sair do loop, vamos devolver cuidadosamente os recursos ao sistema!
     cap.release()
     cv.destroyAllWindows()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
